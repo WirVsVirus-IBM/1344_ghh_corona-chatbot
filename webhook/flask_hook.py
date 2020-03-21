59992b754c91a8d88a99cd2652e1c9b0b33874c8
grotepfn <36507014+grotepfn@users.noreply.github.com>
2020-03-21 16:39:00 +0100

    Create flask_hook.py
    Added webhook

---
+++ webhook/flask_hook.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import jsonify, request
+import requests
+import json
+app = Flask(__name__)
+
+
+req_qa = {
+    "questions": [
+        
+    ],
+    "filters": {
+        "lang": "en"
+    },
+    "top_k_retriever": 5
+    }
+url_qa = "http://3.121.62.187:80/models/1/faq-qa"
+params_qa = {}
+
+@app.route("/qa", methods=['GET', 'POST'])
+def getAnswerFromQA():
+    payload = (request.get_data())
+    question = json.loads(payload)
+    
+    req_qa['questions']= [question['question']]
+    response = requests.post(url_qa,params_qa , json=req_qa)
+    data = json.loads(response.content)
+    return {"answer"  : data['results'][0]['answers'][0]['answer']}
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
